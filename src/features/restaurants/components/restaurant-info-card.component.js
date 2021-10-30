bc3ee4cf7d3e3418b7915bc428442eadcb9c8c3e
Sarthak Kuchhal <developersbeat@gmail.com>
2021-10-31 01:54:00 +0530

    styled components added

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,19 +1,37 @@
 /* eslint-disable prettier/prettier */
 import React from "react";
-import { Text } from "react-native";
+import styled from "styled-components/native";
+import { Text , StyleSheet} from "react-native";
+import { Card } from "react-native-paper";
+
+const Title = styled.Text`
+  padding: 16px;
+  color: red;
+`;
 
 const RestaurantInfoCard = ({ restaurant = {} }) => {
-    const {
+  const {
     name = "Metaverse",
     icon,
-    photos = ["https://static01.nyt.com/images/2020/11/04/dining/04Outdoor5/04Outdoor5-articleLarge-v2.jpg"],
+    photos = [
+      "https://static01.nyt.com/images/2020/11/04/dining/04Outdoor5/04Outdoor5-articleLarge-v2.jpg",
+    ],
     address = "100 random street",
     isOpenNow = true,
     rating = 4,
     isClosedTemporarily,
-    } = restaurant;
-    
-  return <Text>Restaurant Info</Text>;
+  } = restaurant;
+
+  return (
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+      <Title>{name}</Title>
+    </Card>
+  );
 };
 
+const styles = StyleSheet.create({
+  card: { backgroundColor: "white" },
+  cover: { padding: 20, backgroundColor: "white" },
+});
 export default RestaurantInfoCard;
